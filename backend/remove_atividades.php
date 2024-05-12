406402d05bc49e9353e097891f652ebe38e28bb5
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2024-05-12 13:51:13 -0300

    update no create e criação do delete
    melhorei o create dos novos cards e tornei funcional o botão de deletar atividade

---
+++ backend/remove_atividades.php
@@ -0,0 +1,24 @@
+<?php
+// Incluir o arquivo de configuração com a conexão PDO
+require_once("database.php");
+
+// Verificar se o ID da atividade foi enviado
+if(isset($_POST['id'])) {
+    $idAtividade = $_POST['id'];
+    echo $idAtividade;
+
+    $sql = "DELETE FROM tbl_atividade WHERE ATV_ID = :id";
+
+    $stmt = $conexao->prepare($sql);
+    $stmt->bindParam(':id', $idAtividade, PDO::PARAM_INT);
+
+    // Executar a consulta preparada
+    if($stmt->execute()) {
+        echo "sucesso";
+    } else {
+        echo "erro";
+    }
+} else {
+    echo "ID da atividade não recebido";
+}
+?>
