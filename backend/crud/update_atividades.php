3d75eb3f0601d03e9ef762b2f65476c9f09400ad
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2024-05-18 15:27:21 -0300

    detalhes

--- backend/crud/update_atividades.php
+++ backend/crud/update_atividades.php
@@ -8,14 +8,16 @@ var_dump($_POST);
 if(isset($_POST['tituloNovo']) && isset($_POST['descricaoNovo']) && isset($_POST['id'])) {
     $novoTitulo = $_POST['tituloNovo'];
     $novoDescricao = $_POST['descricaoNovo'];
+    $novoConclusao = $_POST['conclusaoNovo'];
     $idAtividade = $_POST['id'];
 
-    $sql = "UPDATE tbl_atividade SET ATV_TITULO = :titulo, ATV_DESCRICAO = :descricao WHERE ATV_ID = :id";
+    $sql = "UPDATE tbl_atividade SET ATV_TITULO = :titulo, ATV_DESCRICAO = :descricao, ATV_DT_CONC = :conclusao WHERE ATV_ID = :id";
 
     $stmt = $conexao->prepare($sql);
     $stmt->bindParam(':titulo', $novoTitulo, PDO::PARAM_STR);
     $stmt->bindParam(':descricao', $novoDescricao, PDO::PARAM_STR);
     $stmt->bindParam(':id', $idAtividade, PDO::PARAM_STR);
+    $stmt->bindParam(':conclusao', $novoConclusao, PDO::PARAM_STR);
 
     if($stmt->execute()) {
         echo "sucesso";
